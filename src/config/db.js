cc23a1766bbd81acb8c5210fede8cfe6194a716e
reetumeen <reetumeen10@gmail.com>
2025-09-29 16:19:04 +0530

    uri changes according to uri string

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGO_URI || 'mongodb+srv://reetumeen10:<yzGB8eWDDuWJVYs3>@cluster1.frfivz0.mongodb.net/courtline?retryWrites=true&w=majority&appName=Cluster1';
+const uri = process.env.MONGO_URI || 'mongodb+srv://reetumeen10:<wQutbMJsjPeJjzcc>@cluster1.frfivz0.mongodb.net/courtline?retryWrites=true&w=majority&appName=Cluster1';
 
 async function connectDB() {
 	try {
 		await mongoose.connect(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
+			// useUnifiedTopology: true,
 		});
 		console.log('Connected to MongoDB');
 	} catch (err) {
